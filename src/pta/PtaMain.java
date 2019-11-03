7fe6ed88c601a90e937d6f2eb95928ffa79a1ffb
WANG Sinan <11849180@mail.sustech.edu.cn>
2019-11-04 00:27:51 +0800

    add stub transformer

--- src/pta/PtaMain.java
+++ src/pta/PtaMain.java
@@ -1,5 +1,7 @@
 package pta;
 
+import soot.PackManager;
+import soot.Transform;
 import soot.options.Options;
 
 import java.nio.file.FileSystems;
@@ -9,16 +11,20 @@ public class PtaMain {
 
     // args[0] test.Hello
     public static void main(String[] args){
-        String classPath = System.getenv("JAVA_HOME");
+        String classPath = System.getenv("JAVA_HOME");  // only test on JDK-1.8 with JAVA_HOME set
         String pathjce = Paths.get(classPath, "jre", "lib", "jce.jar").toAbsolutePath().toString();
         String pathrt = Paths.get(classPath, "jre", "lib", "rt.jar").toAbsolutePath().toString();
         String pathcw = FileSystems.getDefault().getPath("src").toAbsolutePath().toString();
         String cp = String.join(";", pathjce, pathrt, pathcw);
         Options.v().set_soot_classpath(cp);
+        Options.v().set_whole_program(true);
+        Options.v().set_output_format(Options.output_format_jimple);
+        Options.v().setPhaseOption("cg.cha", "enabled:true");
 
-        soot.Main.main(new String[]{
-                "-f", "jimple", args[0]
-        });
+        PackManager.v().getPack("wjtp").add(
+                new Transform("wjtp.pta", new PtaTransformer())
+        );
+        soot.Main.main(args);
     }
 
 }
---
+++ src/pta/PtaTransformer.java
@@ -0,0 +1,12 @@
+package pta;
+
+import soot.SceneTransformer;
+
+import java.util.Map;
+
+public class PtaTransformer extends SceneTransformer {
+    @Override
+    protected void internalTransform(String s, Map<String, String> map) {
+        System.err.println("todo here");
+    }
+}
